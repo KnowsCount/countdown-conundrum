457498ecfcd805b6a6366d7d3bc87d5bc3a630b8
KnowsCount <knowscount@gmail.com>
2024-02-12 00:42:29 +0800

    feat: maths game completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import {
 	MainContainer,
-	LetterBoxes,
-	LetterBox,
 	ButtonWrapper,
 	Button,
 } from '@/styles'
 import { Clock } from '@/components/common/Clock'
 import { Footer } from '@/components/common/Footer'
+import WordsGame from '@/components/modes/word/Wordmode'
+import NumbersGame from '@/components/modes/numbers/Numbersmode'
 
 const IndexPage: React.FC = () => {
-	const [wordList, setWordList] = useState<string[]>([])
+    const [wordList, setWordList] = useState<string[]>([])
 	const [countdownInterval, setCountdownInterval] =
 		useState<NodeJS.Timeout | null>(null)
 	const [timeLeft, setTimeLeft] = useState<number>(30)
@@ -18,10 +18,8 @@ const IndexPage: React.FC = () => {
 	const [inputWord, setInputWord] = useState<string>('')
 	const [message, setMessage] = useState<string>('')
 	const [audio, setAudio] = useState<HTMLAudioElement | null>(null)
-	// Add gameMode state
 	const [gameMode, setGameMode] = useState<string>('word')
 
-	// Add function to switch game mode
 	const switchGameMode = () => {
 		setGameMode(gameMode === 'word' ? 'numbers' : 'word')
 	}
@@ -97,18 +95,20 @@ const IndexPage: React.FC = () => {
 	}
 
 	const startCountdown = () => {
-		setTimeLeft(30)
-		const interval = setInterval(() => {
-			setTimeLeft((timeLeft) => timeLeft - 1)
-		}, 1000)
-		setCountdownInterval(interval)
-
-		// play the audio!
-		const audio = new Audio('/clockonly.mp3')
-		audio.play()
-		setAudio(audio)
+		if (!countdownInterval) {
+			setTimeLeft(30)
+			const interval = setInterval(() => {
+				setTimeLeft((timeLeft) => timeLeft - 1)
+			}, 1000)
+			setCountdownInterval(interval)
+	
+			// play the audio!
+			const audio = new Audio('/clockonly.mp3')
+			audio.play()
+			setAudio(audio)
+		}
 	}
-
+	
 	const canFormWordFromLetters = (word: string, letters: string) => {
 		let letterCounts: { [key: string]: number } = {}
 		for (let letter of letters) {
@@ -170,56 +170,35 @@ const IndexPage: React.FC = () => {
 		}
 	}
 
-	return (
-		<MainContainer>
-			<Clock timeLeft={timeLeft} />
-			<p>Time left: {timeLeft} seconds</p>
-			<h1>Countdown Conundrum</h1>
-			{gameMode === 'word' ? (
-				<>
-					<LetterBoxes>
-						{Array(9)
-							.fill('')
-							.map((_, i) => (
-								<LetterBox key={i}>{letters[i] || ''}</LetterBox>
-							))}
-					</LetterBoxes>
-					<ButtonWrapper>
-						<Button onClick={() => addLetter('vowel')}>Add Vowel</Button>
-					</ButtonWrapper>
-					<ButtonWrapper>
-						<Button onClick={() => addLetter('consonant')}>
-							Add Consonant
-						</Button>
-					</ButtonWrapper>
-					<input
-						type="text"
-						value={inputWord}
-						onChange={(e) => setInputWord(e.target.value)}
-						placeholder="Enter your word here"
-					/>
-					<ButtonWrapper>
-						<Button onClick={checkWord} disabled={letters.length < 9}>
-							Submit
-						</Button>
-					</ButtonWrapper>
-				</>
-			) : (
-				// TODO: Replace this with your numbers game component
-				<p>Numbers game goes here</p>
-			)}
-			<ButtonWrapper>
-				<Button onClick={playGame}>New Game</Button>
-			</ButtonWrapper>
-			<ButtonWrapper>
-				<Button onClick={switchGameMode}>
-					Switch to {gameMode === 'word' ? 'Numbers' : 'Word'} Mode
-				</Button>
-			</ButtonWrapper>
-			<p>{message}</p>
-			<Footer />
-		</MainContainer>
-	)
+    return (
+        <MainContainer>
+            <Clock timeLeft={timeLeft} />
+            <p>Time left: {timeLeft} seconds</p>
+            <h1>Countdown Conundrum</h1>
+            {gameMode === 'word' ? (
+                <WordsGame
+                    timeLeft={timeLeft}
+                    letters={letters}
+                    inputWord={inputWord}
+                    setInputWord={setInputWord}
+                    checkWord={checkWord}
+                    addLetter={addLetter}
+                />
+            ) : (
+				<NumbersGame startCountdown={startCountdown} timeLeft={timeLeft} />
+            )}
+            <ButtonWrapper>
+                <Button onClick={playGame}>New Game</Button>
+            </ButtonWrapper>
+            <ButtonWrapper>
+                <Button onClick={switchGameMode}>
+                    Switch to {gameMode === 'word' ? 'Numbers' : 'Word'} Mode
+                </Button>
+            </ButtonWrapper>
+            <p>{message}</p>
+            <Footer />
+        </MainContainer>
+    )
 }
 
 export default IndexPage
