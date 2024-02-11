97586836530af218a1eb136a99d3b99594ec505b
KnowsCount <knowscount@gmail.com>
2024-02-11 22:59:06 +0800

    style: code linting with prettier (#1)
    * refactor: remove unnecessary config
    
    * style: code linting with prettier

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,47 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 const ClockContainer = styled.div`
-  position: relative;
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
-  border-radius: 50%;
-`;
+	position: relative;
+	width: 100px;
+	height: 100px;
+	border: 1px solid black;
+	border-radius: 50%;
+`
 
 const Hand = styled.div`
-  position: absolute;
-  bottom: 50%;
-  left: 50%;
-  width: 2px;
-  height: 50%;
-  background: black;
-  transform-origin: center bottom;
-  transition: all 1s linear;
-`;
+	position: absolute;
+	bottom: 50%;
+	left: 50%;
+	width: 2px;
+	height: 50%;
+	background: black;
+	transform-origin: center bottom;
+	transition: all 1s linear;
+`
 
 interface ClockProps {
-  timeLeft: number;
+	timeLeft: number
 }
 
 const Clock: React.FC<ClockProps> = ({ timeLeft }) => {
-  const [degrees, setDegrees] = useState(0);
+	const [degrees, setDegrees] = useState(0)
 
-  useEffect(() => {
-    // Map the time left (30 to 0 seconds) to degrees (0 to 180 degrees)
-    const newDegrees = mapRange(timeLeft, 30, 0, 0, 180);
-    setDegrees(newDegrees);
-  }, [timeLeft]);
+	useEffect(() => {
+		// Map the time left (30 to 0 seconds) to degrees (0 to 180 degrees)
+		const newDegrees = mapRange(timeLeft, 30, 0, 0, 180)
+		setDegrees(newDegrees)
+	}, [timeLeft])
 
-  function mapRange(value: number, a: number, b: number, c: number, d: number) {
-    return (value - a) * (d - c) / (b - a) + c;
-  }
+	function mapRange(
+		value: number,
+		a: number,
+		b: number,
+		c: number,
+		d: number
+	) {
+		return ((value - a) * (d - c)) / (b - a) + c
+	}
 
-  return (
-    <ClockContainer>
-      <Hand style={{ transform: `rotate(${degrees}deg)` }} />
-    </ClockContainer>
-  );
+	return (
+		<ClockContainer>
+			<Hand style={{ transform: `rotate(${degrees}deg)` }} />
+		</ClockContainer>
+	)
 }
 
-export default Clock;
+export default Clock
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,176 +1,206 @@
-import React, { useState, useEffect } from 'react';
-import { MainContainer, LetterBoxes, LetterBox, ButtonWrapper, Button } from '@/styles';
-import Clock from '@/components/Clock';
-import Footer from "@/components/Footer";
+import React, { useState, useEffect } from 'react'
+import {
+	MainContainer,
+	LetterBoxes,
+	LetterBox,
+	ButtonWrapper,
+	Button,
+} from '@/styles'
+import Clock from '@/components/Clock'
+import Footer from '@/components/Footer'
 
 const IndexPage: React.FC = () => {
-    const [wordList, setWordList] = useState<string[]>([]);
-    const [countdownInterval, setCountdownInterval] = useState<NodeJS.Timeout | null>(null);
-    const [timeLeft, setTimeLeft] = useState<number>(30);
-    const [letters, setLetters] = useState<string>('');
-    const [inputWord, setInputWord] = useState<string>('');
-    const [message, setMessage] = useState<string>('');
-    const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+	const [wordList, setWordList] = useState<string[]>([])
+	const [countdownInterval, setCountdownInterval] =
+		useState<NodeJS.Timeout | null>(null)
+	const [timeLeft, setTimeLeft] = useState<number>(30)
+	const [letters, setLetters] = useState<string>('')
+	const [inputWord, setInputWord] = useState<string>('')
+	const [message, setMessage] = useState<string>('')
+	const [audio, setAudio] = useState<HTMLAudioElement | null>(null)
 
-    useEffect(() => {
-        fetch('https://random-word-api.herokuapp.com/all')
-            .then(response => response.json())
-            .then(data => {
-                setWordList(data);
-            });
-    }, []);
+	useEffect(() => {
+		fetch('https://random-word-api.herokuapp.com/all')
+			.then((response) => response.json())
+			.then((data) => {
+				setWordList(data)
+			})
+	}, [])
 
-    useEffect(() => {
-      if (timeLeft > 0 && countdownInterval) {
-            const id = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
-            return () => clearTimeout(id);
-      } else if (timeLeft <= 0 && countdownInterval) {
-            clearInterval(countdownInterval);
-            setCountdownInterval(null);
-            setMessage('Time is up!');
-            setInputWord('');
-            if (audio) {
-                audio.pause();
-                setAudio(null);
-            }
-      }
-  }, [timeLeft]);
-  
+	useEffect(() => {
+		if (timeLeft > 0 && countdownInterval) {
+			const id = setTimeout(() => setTimeLeft(timeLeft - 1), 1000)
+			return () => clearTimeout(id)
+		} else if (timeLeft <= 0 && countdownInterval) {
+			clearInterval(countdownInterval)
+			setCountdownInterval(null)
+			setMessage('Time is up!')
+			setInputWord('')
+			if (audio) {
+				audio.pause()
+				setAudio(null)
+			}
+		}
+	}, [timeLeft])
 
-    const generateRandomVowel = () => {
-        const vowels = 'aeiou';
-        return vowels[Math.floor(Math.random() * vowels.length)];
-    }
+	const generateRandomVowel = () => {
+		const vowels = 'aeiou'
+		return vowels[Math.floor(Math.random() * vowels.length)]
+	}
 
-    const generateRandomConsonant = () => {
-        const consonants = 'bcdfghjklmnpqrstvwxyz';
-        return consonants[Math.floor(Math.random() * consonants.length)];
-    }
+	const generateRandomConsonant = () => {
+		const consonants = 'bcdfghjklmnpqrstvwxyz'
+		return consonants[Math.floor(Math.random() * consonants.length)]
+	}
 
-    useEffect(() => {
-      if (letters.length === 9) {
-          startCountdown();
-      }
-  }, [letters]);
-  
-  const addLetter = (type: string) => {
-      if (letters.length >= 9) {
-            setMessage('You already have 9 letters.');
-            return;
-      }
-  
-      let newLetter;
-      if (type === 'vowel') {
-          newLetter = generateRandomVowel();
-      } else {
-          newLetter = generateRandomConsonant();
-      }
-      setLetters(letters + newLetter);
-    }
+	useEffect(() => {
+		if (letters.length === 9) {
+			startCountdown()
+		}
+	}, [letters])
 
-    const playGame = () => {
-        setLetters('');
-        setInputWord('');
-        setMessage('');
-        setTimeLeft(30); 
-        if (countdownInterval) {
-            clearInterval(countdownInterval);
-            setCountdownInterval(null);
-        }
-        if (audio) {
-            audio.pause();
-            setAudio(null);
-        }
-    }
+	const addLetter = (type: string) => {
+		if (letters.length >= 9) {
+			setMessage('You already have 9 letters.')
+			return
+		}
 
-    const startCountdown = () => {
-        setTimeLeft(30);
-        const interval = setInterval(() => {
-            setTimeLeft(timeLeft => timeLeft - 1);
-        }, 1000);
-        setCountdownInterval(interval);
+		let newLetter
+		if (type === 'vowel') {
+			newLetter = generateRandomVowel()
+		} else {
+			newLetter = generateRandomConsonant()
+		}
+		setLetters(letters + newLetter)
+	}
 
-        // play the audio!
-        const audio = new Audio('/clockonly.mp3');
-        audio.play();
-        setAudio(audio);
-    }
+	const playGame = () => {
+		setLetters('')
+		setInputWord('')
+		setMessage('')
+		setTimeLeft(30)
+		if (countdownInterval) {
+			clearInterval(countdownInterval)
+			setCountdownInterval(null)
+		}
+		if (audio) {
+			audio.pause()
+			setAudio(null)
+		}
+	}
 
-    const canFormWordFromLetters = (word: string, letters: string) => {
-        let letterCounts: { [key: string]: number } = {};
-        for (let letter of letters) {
-            if (letter in letterCounts) {
-                letterCounts[letter]++;
-            } else {
-                letterCounts[letter] = 1;
-            }
-        }
-        for (let letter of word) {
-            if (!(letter in letterCounts) || letterCounts[letter] == 0) {
-                return false;
-            } else {
-                letterCounts[letter]--;
-            }
-        }
-        return true;
-    }
+	const startCountdown = () => {
+		setTimeLeft(30)
+		const interval = setInterval(() => {
+			setTimeLeft((timeLeft) => timeLeft - 1)
+		}, 1000)
+		setCountdownInterval(interval)
 
-    const findLongestWord = (letters: string) => {
-        let longestWord = '';
-        for (let word of wordList) {
-            if (word.length > longestWord.length && canFormWordFromLetters(word, letters)) {
-                longestWord = word;
-            }
-        }
-        return longestWord;
-    }
+		// play the audio!
+		const audio = new Audio('/clockonly.mp3')
+		audio.play()
+		setAudio(audio)
+	}
 
-    const checkWord = () => {
-        if (countdownInterval) {
-            clearInterval(countdownInterval);
-            setCountdownInterval(null);
-        }
-        if (audio) {
-            audio.pause();
-            setAudio(null);
-        }
-        const longestWord = findLongestWord(letters);
-        if (inputWord.length > letters.length) {
-            setMessage('Your word is too long.');
-        } else if (!canFormWordFromLetters(inputWord, letters)) {
-            setMessage('Your word contains letters not in the given set.');
-        } else if (!wordList.includes(inputWord)) {
-            setMessage(`Sorry, ${inputWord} is not a valid word.`);
-        } else {
-            if (inputWord.length === longestWord.length) {
-                setMessage(`Well done! Your word was: ${inputWord}. That's the longest word possible!`);
-            } else {
-                setMessage(`Well done! Your word was: ${inputWord}. But you could have done better. The longest word possible is: ${longestWord}`);
-            }
-        }
-    }
+	const canFormWordFromLetters = (word: string, letters: string) => {
+		let letterCounts: { [key: string]: number } = {}
+		for (let letter of letters) {
+			if (letter in letterCounts) {
+				letterCounts[letter]++
+			} else {
+				letterCounts[letter] = 1
+			}
+		}
+		for (let letter of word) {
+			if (!(letter in letterCounts) || letterCounts[letter] == 0) {
+				return false
+			} else {
+				letterCounts[letter]--
+			}
+		}
+		return true
+	}
 
-    return (
-      <MainContainer>
-        <Clock timeLeft={timeLeft} />
-        <p>Time left: {timeLeft} seconds</p>
-        <h1>Countdown Conundrum</h1>
-        <LetterBoxes>
-            {Array(9).fill('').map((_, i) => (
-                <LetterBox key={i}>
-                    {letters[i] || ''}
-                </LetterBox>
-            ))}
-        </LetterBoxes>
-        <ButtonWrapper><Button onClick={() => addLetter('vowel')}>Add Vowel</Button></ButtonWrapper>
-        <ButtonWrapper><Button onClick={() => addLetter('consonant')}>Add Consonant</Button></ButtonWrapper>
-        <input type="text" value={inputWord} onChange={e => setInputWord(e.target.value)} placeholder="Enter your word here" />
-        <ButtonWrapper><Button onClick={checkWord} disabled={letters.length < 9}>Submit</Button></ButtonWrapper>
-        <ButtonWrapper><Button onClick={playGame}>New Game</Button></ButtonWrapper>
-        <p>{message}</p>
-        <Footer />
-      </MainContainer>
-    );
-};
+	const findLongestWord = (letters: string) => {
+		let longestWord = ''
+		for (let word of wordList) {
+			if (
+				word.length > longestWord.length &&
+				canFormWordFromLetters(word, letters)
+			) {
+				longestWord = word
+			}
+		}
+		return longestWord
+	}
 
-export default IndexPage;
+	const checkWord = () => {
+		if (countdownInterval) {
+			clearInterval(countdownInterval)
+			setCountdownInterval(null)
+		}
+		if (audio) {
+			audio.pause()
+			setAudio(null)
+		}
+		const longestWord = findLongestWord(letters)
+		if (inputWord.length > letters.length) {
+			setMessage('Your word is too long.')
+		} else if (!canFormWordFromLetters(inputWord, letters)) {
+			setMessage('Your word contains letters not in the given set.')
+		} else if (!wordList.includes(inputWord)) {
+			setMessage(`Sorry, ${inputWord} is not a valid word.`)
+		} else {
+			if (inputWord.length === longestWord.length) {
+				setMessage(
+					`Well done! Your word was: ${inputWord}. That's the longest word possible!`
+				)
+			} else {
+				setMessage(
+					`Well done! Your word was: ${inputWord}. But you could have done better. The longest word possible is: ${longestWord}`
+				)
+			}
+		}
+	}
+
+	return (
+		<MainContainer>
+			<Clock timeLeft={timeLeft} />
+			<p>Time left: {timeLeft} seconds</p>
+			<h1>Countdown Conundrum</h1>
+			<LetterBoxes>
+				{Array(9)
+					.fill('')
+					.map((_, i) => (
+						<LetterBox key={i}>{letters[i] || ''}</LetterBox>
+					))}
+			</LetterBoxes>
+			<ButtonWrapper>
+				<Button onClick={() => addLetter('vowel')}>Add Vowel</Button>
+			</ButtonWrapper>
+			<ButtonWrapper>
+				<Button onClick={() => addLetter('consonant')}>
+					Add Consonant
+				</Button>
+			</ButtonWrapper>
+			<input
+				type="text"
+				value={inputWord}
+				onChange={(e) => setInputWord(e.target.value)}
+				placeholder="Enter your word here"
+			/>
+			<ButtonWrapper>
+				<Button onClick={checkWord} disabled={letters.length < 9}>
+					Submit
+				</Button>
+			</ButtonWrapper>
+			<ButtonWrapper>
+				<Button onClick={playGame}>New Game</Button>
+			</ButtonWrapper>
+			<p>{message}</p>
+			<Footer />
+		</MainContainer>
+	)
+}
+
+export default IndexPage
